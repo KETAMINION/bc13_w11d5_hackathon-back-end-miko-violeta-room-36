4a52694cfc6ac383b18ee97727b4fddd65498e6d
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-12-09 11:27:05 +0000

    backend clean

--- models/index.js
+++ models/index.js
@@ -5,14 +5,6 @@ export async function getYoga() {
   return subjectArray.rows;
 }
 
-export async function updateDataModels(data) {
-  const subjectArray = await pool.query(
-    `INSERT INTO progressCheck (user_id, day, subject) VALUES (1, $1, $2)`,
-    [data.day, data.subject]
-  );
-  return subjectArray;
-}
-
 export async function searchYogaById(id) {
   const result = await pool.query('SELECT * FROM yoga WHERE id = $1', [id]);
   console.log('searchYogaById function is working!');
--- routers/index.js
+++ routers/index.js
@@ -1,14 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { getYoga, updateDataModels, searchYogaById } from '../models/index.js';
+import { getYoga, searchYogaById } from '../models/index.js';
 
 router.get('/', async function (req, res) {
   const result = await getYoga();
-  return res.json({ success: true, payload: result });
-});
-
-router.post('/', async function (req, res) {
-  const result = await updateDataModels(req.body);
   return res.json({ success: true, payload: result });
 });
 
