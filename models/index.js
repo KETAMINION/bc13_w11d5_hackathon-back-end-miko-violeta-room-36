e4b7f53ea0225134985d7f78b53286df52dc73df
violetacf <violeta@cique.es>
2022-12-09 11:03:15 +0000

    get by id working

--- models/index.js
+++ models/index.js
@@ -1,14 +1,20 @@
-import {pool} from "../db/index.js"
+import { pool } from '../db/index.js';
 
-export async function getYoga (){
-    const subjectArray = await pool.query ("SELECT * FROM yoga")
-    return subjectArray.rows
+export async function getYoga() {
+  const subjectArray = await pool.query('SELECT * FROM yoga');
+  return subjectArray.rows;
 }
 
-export async function updateDataModels (data){
-    const subjectArray = await pool.query (`INSERT INTO progressCheck (user_id, day, subject) VALUES (1, $1, $2)`, [data.day, data.subject])
-    return subjectArray
+export async function updateDataModels(data) {
+  const subjectArray = await pool.query(
+    `INSERT INTO progressCheck (user_id, day, subject) VALUES (1, $1, $2)`,
+    [data.day, data.subject]
+  );
+  return subjectArray;
 }
 
-
-
+export async function searchYogaById(id) {
+  const result = await pool.query('SELECT * FROM yoga WHERE id = $1', [id]);
+  console.log('searchYogaById function is working!');
+  return result.rows[0];
+}
--- routers/index.js
+++ routers/index.js
@@ -1,16 +1,20 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import { getYoga, updateDataModels } from "../models/index.js"
+import { getYoga, updateDataModels, searchYogaById } from '../models/index.js';
 
-router.get("/", async function(req, res) {
-    const result = await getYoga()
-    return res.json({success: true, payload: result})
-})
+router.get('/', async function (req, res) {
+  const result = await getYoga();
+  return res.json({ success: true, payload: result });
+});
 
-router.post("/", async function(req,res) {
-    const result = await updateDataModels(req.body)
-    return res.json({success: true, payload: result}) 
-})
+router.post('/', async function (req, res) {
+  const result = await updateDataModels(req.body);
+  return res.json({ success: true, payload: result });
+});
 
+router.get('/:id', async function (req, res) {
+  const result = await searchYogaById(req.params.id);
+  return res.json({ success: true, payload: result });
+});
 
-export default router
+export default router;
